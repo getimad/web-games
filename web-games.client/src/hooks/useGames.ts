264e39c8ced8e8c046e857d9ff88361d69f91ee3
Imad Ez-zahi <107067409+getimad@users.noreply.github.com>
2023-12-29 16:26:19 +0100

    Create new custom hook useGames.ts
    - Add 3 interfaces (Game, Platform and FetchGamesResponse).
    - Add useGames hook to fetch games and return games and error.

---
+++ web-games.client/src/hooks/useGames.ts
@@ -0,0 +1,45 @@
+import { useEffect, useState } from 'react';
+import apiClient from '../services/api-client';
+import { CanceledError } from 'axios';
+
+export interface Platform {
+  id: number;
+  name: string;
+  slug: string;
+}
+
+export interface Game {
+  id: number;
+  name: string;
+  background_image: string;
+  parent_platforms: { platform: Platform }[];
+}
+
+interface FetchGamesResponse {
+  count: number;
+  results: Game[];
+}
+
+function useGames() {
+
+  const [games, setGames] = useState<Game[]>([]);
+  const [error, setError] = useState('');
+
+  useEffect(() => {
+    const controller = new AbortController();
+
+    apiClient.get<FetchGamesResponse>('/games', { signal: controller.signal })
+      .then(res => setGames(res.data.results))
+      .catch(err => {
+        if (err instanceof CanceledError)
+          return;
+        setError(err.message);
+      });
+
+    return () => controller.abort();
+  }, []);
+
+  return { games, error };
+}
+
+export default useGames;

26aaacfe8528e248ecfaec7cb1619785dbd6e0b1
Imad Ez-zahi <107067409+getimad@users.noreply.github.com>
2023-12-30 00:32:33 +0100

    Add GameCardSkeleton to GameGrid.tsx
    And this will generate 6 GameCardSkeleton when the data are not ready.

--- web-games.client/src/components/GameGrid.tsx
+++ web-games.client/src/components/GameGrid.tsx
@@ -1,16 +1,17 @@
 import { SimpleGrid, Text } from '@chakra-ui/react';
 import useGames from '../hooks/useGames';
 import GameCard from './GameCard';
-
+import GameCardSkeleton from './GameCardSkeleton';
 
 function GameGrid() {
-  const { games, error } = useGames();
+  const { games, error, isLoading } = useGames();
 
   return (
     <>
       {error && <Text>{error}</Text>}
-      <SimpleGrid columns={{ sm: 1, md:2, lg: 3}} spacing={3} padding={3}>
-        {games.map(game => <GameCard key={game.id} game={game} />)}
+      <SimpleGrid columns={{ sm: 2, lg: 3 }} spacing={3} padding={3}>
+        {!isLoading || Array.from({length: 6 }).map((_, index) => <GameCardSkeleton key={index} />)}
+        {games.map(game => <GameCard key={game.id} game={game} />)}*
       </SimpleGrid>
     </>
   );

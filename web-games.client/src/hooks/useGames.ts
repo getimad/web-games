5136b51e2e8b5df3ff818ed6d59ca08c04a5e6bb
Imad Ez-zahi <107067409+getimad@users.noreply.github.com>
2023-12-30 00:14:34 +0100

    Add isLoading state to useGames.ts hook
    isLoading state will target the date, if it is reading or not.

--- web-games.client/src/hooks/useGames.ts
+++ web-games.client/src/hooks/useGames.ts
@@ -22,25 +22,31 @@ interface FetchGamesResponse {
 }
 
 function useGames() {
-
   const [games, setGames] = useState<Game[]>([]);
   const [error, setError] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
     const controller = new AbortController();
 
+    setIsLoading(true);
+
     apiClient.get<FetchGamesResponse>('/games', { signal: controller.signal })
-      .then(res => setGames(res.data.results))
+      .then(res => {
+        setGames(res.data.results);
+        setIsLoading(false);
+      })
       .catch(err => {
         if (err instanceof CanceledError)
           return;
         setError(err.message);
+        setIsLoading(false);
       });
 
     return () => controller.abort();
   }, []);
 
-  return { games, error };
+  return { games, error, isLoading };
 }
 
 export default useGames;

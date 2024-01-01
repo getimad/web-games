031dc7bc842217b0333a53cc84ed3ad6c7f23dfc
Imad Ez-zahi <107067409+getimad@users.noreply.github.com>
2024-01-01 21:47:05 +0100

    Fix main section when click in another genre
    The previous games state is still on the page when it is loading.

--- web-games.client/src/components/GameGrid.tsx
+++ web-games.client/src/components/GameGrid.tsx
@@ -16,8 +16,9 @@ function GameGrid({ selectedGenre }: Props) {
       {error && <Text>{error}</Text>}
       <Text fontSize='5xl' fontWeight={700} paddingBottom={6}>Top Games</Text>
       <SimpleGrid templateColumns='repeat(auto-fill, minmax(300px, 1fr))' spacing={3}>
-        {isLoading && Array.from({length: 6 }).map((_, index) => <GameCardSkeleton key={index} />)}
-        {data.map(game => <GameCard key={game.id} game={game} />)}
+        {isLoading
+          ? Array.from({ length: 15 }).map((_, index) => <GameCardSkeleton key={index} />)
+          : data.map(game => <GameCard key={game.id} game={game} />)}
       </SimpleGrid>
     </Box>
   );

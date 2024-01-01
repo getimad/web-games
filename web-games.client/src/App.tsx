aab51c5ac8d3c460ac380d2570cca0e37f7ba578
Imad Ez-zahi <107067409+getimad@users.noreply.github.com>
2024-01-01 18:25:52 +0100

    Refactor App.tsx
    - Add GenreList to the Aside section.
    - Redesign the template of the page (Add some margin and...).

--- web-games.client/src/App.tsx
+++ web-games.client/src/App.tsx
@@ -1,18 +1,28 @@
 import { Grid, GridItem, Show } from '@chakra-ui/react';
 import NavBar from './components/NavBar';
 import GameGrid from './components/GameGrid';
+import GenreList from './components/GenreList';
 
 function App() {
   return (
-    <Grid templateAreas={{
-      base: `'nav' 'main'`,
-      lg: `'nav nav' 'aside main'`
-    }}>
+    <Grid
+      templateAreas={{
+        base: `'nav' 'main'`,
+        lg: `'nav nav' 'aside main'`
+      }}
+      gridTemplateColumns={{
+        base: '1fr',
+        lg: '250px 1fr'
+      }}
+      marginX={5}
+    >
       <GridItem area='nav'>
         <NavBar />
       </GridItem>
       <Show above='lg'>
-        <GridItem area='aside'>Aside</GridItem>
+        <GridItem area='aside'>
+          <GenreList />
+        </GridItem>
       </Show>
       <GridItem area='main'>
         <GameGrid />

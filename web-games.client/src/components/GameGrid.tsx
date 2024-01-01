f84711eda17e6083c2ed5513a9625220f692aeba
Imad Ez-zahi <107067409+getimad@users.noreply.github.com>
2024-01-01 18:56:48 +0100

    Refactor GameGrid.tsx
    - Add Title of the list.
    - Put all children of the component inside Box component.
    - Redesign the look of the list.

--- web-games.client/src/components/GameGrid.tsx
+++ web-games.client/src/components/GameGrid.tsx
@@ -1,19 +1,20 @@
-import { SimpleGrid, Text } from '@chakra-ui/react';
+import { SimpleGrid, Text, Box } from '@chakra-ui/react';
 import useGames from '../hooks/useGames';
 import GameCard from './GameCard';
 import GameCardSkeleton from './GameCardSkeleton';
 
 function GameGrid() {
-  const { games, error, isLoading } = useGames();
+  const { data, error, isLoading } = useGames();
 
   return (
-    <>
+    <Box>
       {error && <Text>{error}</Text>}
-      <SimpleGrid columns={{ sm: 2, lg: 3 }} spacing={3} padding={3}>
-        {!isLoading || Array.from({length: 6 }).map((_, index) => <GameCardSkeleton key={index} />)}
-        {games.map(game => <GameCard key={game.id} game={game} />)}*
+      <Text fontSize='5xl' fontWeight={700} paddingBottom={6}>Top Games</Text>
+      <SimpleGrid templateColumns='repeat(auto-fill, minmax(300px, 1fr))' spacing={3}>
+        {isLoading && Array.from({length: 6 }).map((_, index) => <GameCardSkeleton key={index} />)}
+        {data.map(game => <GameCard key={game.id} game={game} />)}
       </SimpleGrid>
-    </>
+    </Box>
   );
 }
 

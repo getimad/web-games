987a54d44b6f6af5752d9840039d3cf496dbbc95
Imad Ez-zahi <107067409+getimad@users.noreply.github.com>
2024-01-01 16:37:52 +0100

    Refactor getCroppedImageUrl in get-cropped-image-url.ts
    Make this function reusable for all types of images in the project.

--- web-games.client/src/services/get-cropped-image-url.ts
+++ web-games.client/src/services/get-cropped-image-url.ts
@@ -1,5 +1,8 @@
 const getCroppedImageUrl = (url: string) => {
-  const newUrl = 'https://media.rawg.io/media/crop/600/400/' + url.slice(url.indexOf('games'));
+  const target = '/media/';
+  const index = url.indexOf(target) + target.length - 1;
+
+  const newUrl = url.slice(0, index) + '/crop/600/400/' + url.slice(index);
   return newUrl;
 };
 

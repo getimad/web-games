d907062b32802196c9efc66bd36c30ee1a6c304b
Imad Ez-zahi <107067409+getimad@users.noreply.github.com>
2024-01-05 19:58:41 +0100

    Clean up GameCard and change style

--- web-games.client/src/components/GameCard.tsx
+++ web-games.client/src/components/GameCard.tsx
@@ -13,12 +13,12 @@ function GameCard({ game }: Props) {
   return (
     <Card variant='filled' borderRadius={10} overflow='hidden'>
       <Image minHeight={200} src={game.background_image === null ? nullBackgroundImage : getCroppedImageUrl(game.background_image)} />
-      <CardBody>
-        <Heading fontSize='lg'>{game.name}</Heading>
+      <CardBody paddingY={2}>
         <HStack alignItems='center' justifyContent='space-between'>
           <PlatformIconList platforms={game.parent_platforms.map(p => p.platform)} />
           <CriticScore score={game.metacritic} />
         </HStack>
+        <Heading fontSize='lg' paddingTop={1}>{game.name}</Heading>
       </CardBody>
     </Card>
   );

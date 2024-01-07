b746dc3562daf59ca9ca9d3c1c090c0350d65942
Imad Ez-zahi <107067409+getimad@users.noreply.github.com>
2024-01-07 16:18:09 +0100

    Implement dark mode by using theme.ts

--- web-games.client/src/main.tsx
+++ web-games.client/src/main.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { ChakraProvider } from '@chakra-ui/react';
+import { ChakraProvider, ColorModeScript } from '@chakra-ui/react';
 import App from './App.tsx';
+import theme from './theme.ts';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
+      <ColorModeScript initialColorMode={theme.config.initialColorMode} />
       <App />
     </ChakraProvider>
   </React.StrictMode>,

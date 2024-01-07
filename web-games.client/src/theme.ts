f48ac6a8e149b783c967dc2833f8991400d23968
Imad Ez-zahi <107067409+getimad@users.noreply.github.com>
2024-01-07 17:09:42 +0100

    Add custom gray color to theme.ts

--- web-games.client/src/theme.ts
+++ web-games.client/src/theme.ts
@@ -4,6 +4,22 @@ const config: ThemeConfig = {
   initialColorMode: 'dark'
 };
 
-const theme = extendTheme({ config });
+const theme = extendTheme({
+  config,
+  colors: {
+    gray: {
+      50: '#F9F9F9',
+      100: '#EDEDED',
+      200: '#D3D3D3',
+      300: '#B3B3B3',
+      400: '#A0A0A0',
+      500: '#898989',
+      600: '#6c6c6c',
+      700: '#202020',
+      800: '#121212',
+      900: '#111',
+    }
+  }
+});
 
 export default theme;

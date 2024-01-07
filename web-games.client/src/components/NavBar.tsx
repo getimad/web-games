355d373567942ea33e10b297dd48e0e08b2d302d
Imad Ez-zahi <107067409+getimad@users.noreply.github.com>
2024-01-07 16:47:39 +0100

    Add ToggleThemeSwitch to the NavBar.tsx component

--- web-games.client/src/components/NavBar.tsx
+++ web-games.client/src/components/NavBar.tsx
@@ -1,5 +1,6 @@
 import { Box, Flex, Image, Spacer } from '@chakra-ui/react';
 import logo from '../assets/getimad-logo-white.svg';
+import ToggleThemeSwitch from './ToggleThemeSwitch';
 
 function NavBar() {
   return (
@@ -7,6 +8,7 @@ function NavBar() {
       <Flex h={16} alignItems='center' justifyContent='space-between'>
         <Image src={logo} boxSize={12} />
         <Spacer />
+        <ToggleThemeSwitch />
       </Flex>
     </Box>
   );

0c2a4ab2f42578c91fd1515f794ded2cff1c2f73
Imad Ez-zahi <107067409+getimad@users.noreply.github.com>
2024-01-08 19:31:57 +0100

    Fix bug NavBar is top of SearchBar

--- web-games.client/src/components/SearchContainer.tsx
+++ web-games.client/src/components/SearchContainer.tsx
@@ -18,16 +18,17 @@ function SearchContainer({ onOpenSearchBox }: Props) {
       position='fixed'
       top='0px'
       right='0px'
-      zIndex='1'
+      zIndex='300'
     >
       <Box
         bg='rgba(0, 0, 0, 0.7)'
         h='100%'
         w='100%'
         position='absolute'
-        zIndex='2'
+        zIndex='301'
+        backdropFilter='saturate(180%) blur(9px)'
         onClick={() => onOpenSearchBox(false)} />
-      <Stack h="60%" w="60%" minH='500px' minW='400px' zIndex='3'>
+      <Stack h="60%" w="60%" minH='500px' minW='400px' zIndex='302'>
         <SearchBox isSearching={isSearching} query={searchQuery} onSearch={query => SetSearchQuery(query)} />
         <SearchResult onReady={isLoading => SetIsSearching(isLoading)} searchQuery={searchQuery} />
       </Stack>

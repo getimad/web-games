d6eecc57817a5958565fb186c2ced98be98db4a1
Imad Ez-zahi <107067409+getimad@users.noreply.github.com>
2024-01-11 16:31:36 +0100

    Add QueryClient and QueryClientProvider to main.tsx

--- web-games.client/src/main.tsx
+++ web-games.client/src/main.tsx
@@ -1,14 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { ChakraProvider, ColorModeScript } from '@chakra-ui/react';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import App from './App.tsx';
 import theme from './theme.ts';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+const query = new QueryClient();
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <ChakraProvider theme={theme}>
       <ColorModeScript initialColorMode={theme.config.initialColorMode} />
-      <App />
+      <QueryClientProvider client={query}>
+        <App />
+      </QueryClientProvider>
     </ChakraProvider>
-  </React.StrictMode>,
+  </React.StrictMode>
 );

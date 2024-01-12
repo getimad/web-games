c5aa11196dcb3369281a73b7ed17aa8557b20b4c
Imad Ez-zahi <107067409+getimad@users.noreply.github.com>
2024-01-12 21:57:13 +0100

    Refactore GameGrid.tsx by separating rendering logic

--- web-games.client/src/components/GameGrid.tsx
+++ web-games.client/src/components/GameGrid.tsx
@@ -11,13 +11,19 @@ interface Props {
 function GameGrid({ gameQuery }: Props) {
   const { data, error, isLoading } = useGames(gameQuery);
 
+  const renderGameCards = () => {
+    if (isLoading) {
+      return Array.from({ length: 15 }).map((_, index) => <GameCardSkeleton key={index} />);
+    }
+
+    return data?.results.map(game => <GameCard key={game.id} game={game} />);
+  }
+
   return (
     <Box>
       {error && <Text>{error.message}</Text>}
       <SimpleGrid templateColumns='repeat(auto-fill, minmax(300px, 1fr))' spacing={3}>
-        {isLoading
-          ? Array.from({ length: 15 }).map((_, index) => <GameCardSkeleton key={index} />)
-          : data?.results.map(game => <GameCard key={game.id} game={game} />)}
+        {renderGameCards()}
       </SimpleGrid>
     </Box>
   );

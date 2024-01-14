35f2ff98823110b17ea87bf5276c1f6bf17fa24b
Imad Ez-zahi <107067409+getimad@users.noreply.github.com>
2024-01-14 18:40:42 +0100

    Add 'scoreData' object to CriticScore component
    Add color and possibilty for score not avalaible. This makes it easier for users to visually distinguish between different score levels.

--- web-games.client/src/components/CriticScore.tsx
+++ web-games.client/src/components/CriticScore.tsx
@@ -5,9 +5,14 @@ interface Props {
 }
 
 function CriticScore({ score }: Props) {
+  const scoreData = {
+    data: score === null ? 'N/A' : score,
+    color: score < 50 ? 'red' : score < 75 ? 'yellow' : 'green'
+  };
+
   return (
-    <Badge variant='solid' textAlign='center' width={10} borderRadius={5}>
-      {score}
+    <Badge variant='solid' textAlign='center' width={10} borderRadius={5} colorScheme={scoreData.color}>
+      {scoreData.data}
     </Badge>
   );
 }

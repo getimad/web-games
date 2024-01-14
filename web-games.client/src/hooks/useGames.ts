319e31ec00f1fc8212bd3d4c6beee273a3f8bb58
Imad Ez-zahi <107067409+getimad@users.noreply.github.com>
2024-01-14 02:51:10 +0100

    Update useGames hook from useQuery to useInfiniteQuery
    This change make the useGames hook to send new request to the server,
    asks for next page of games.

--- web-games.client/src/hooks/useGames.ts
+++ web-games.client/src/hooks/useGames.ts
@@ -1,4 +1,4 @@
-import { useQuery } from '@tanstack/react-query';
+import { useInfiniteQuery } from '@tanstack/react-query';
 import GameQuery from '../interfaces/GameQuery';
 import Game from '../interfaces/Game';
 import FetchResponse from '../interfaces/FetchResponse';
@@ -6,15 +6,21 @@ import APIClient from '../services/api-client';
 
 const apiClient = new APIClient<Game>('/games');
 
-const useGames = (gameQuery: GameQuery) => useQuery<FetchResponse<Game>>({
+const useGames = (gameQuery: GameQuery) => useInfiniteQuery<FetchResponse<Game>>({
   queryKey: ['games', gameQuery],
-  queryFn: () => apiClient.getAll({
+  queryFn: ({ pageParam = 1 }) => apiClient.getAll({
     params: {
+      page_size: 12,
       genres: gameQuery.genre?.id,
       parent_platforms: gameQuery.platform?.id,
-      ordering: gameQuery.order?.value
+      ordering: gameQuery.order?.value,
+      page: pageParam,
     }
   }),
+  initialPageParam: 1,
+  getNextPageParam: (lastPage, allPages) => {
+    return lastPage.next ? allPages.length + 1 : undefined;
+  },
   staleTime: 5 * 60 * 1000  // 5 minutes
 });
 

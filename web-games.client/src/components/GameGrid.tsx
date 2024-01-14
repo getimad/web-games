a02fe866866539375e63254b14281a112b6c89ea
Imad Ez-zahi <107067409+getimad@users.noreply.github.com>
2024-01-14 02:53:56 +0100

    Add infinite scrolling to GameGrid component
    This change make the component render next page of games with showing
    a spinner at the bottom.

--- web-games.client/src/components/GameGrid.tsx
+++ web-games.client/src/components/GameGrid.tsx
@@ -1,22 +1,47 @@
-import { SimpleGrid, Text, Box } from '@chakra-ui/react';
+import { SimpleGrid, Text, Box, Spinner, HStack } from '@chakra-ui/react';
 import useGames from '../hooks/useGames';
 import GameCard from './GameCard';
 import GameCardSkeleton from './GameCardSkeleton';
 import GameQuery from '../interfaces/GameQuery';
+import { useEffect, useRef } from 'react';
 
 interface Props {
   gameQuery: GameQuery;
 }
 
 function GameGrid({ gameQuery }: Props) {
-  const { data, error, isLoading } = useGames(gameQuery);
+  const loadMoreRef = useRef(null);
+  const { data, fetchNextPage, hasNextPage, isLoading, isFetchingNextPage, error } = useGames(gameQuery);
+
+  useEffect(() => {
+    if (isLoading || isFetchingNextPage) return;
+
+    const observe = new IntersectionObserver(
+      entries => {
+        if (entries[0].isIntersecting && hasNextPage) {
+          fetchNextPage();
+        }
+      },
+      { threshold: 1 }
+    );
+
+    const el = loadMoreRef && loadMoreRef.current;
+
+    if (!el) return;
+
+    observe.observe(el);
+
+    return () => observe.unobserve(el);
+  }, [isLoading, isFetchingNextPage, hasNextPage, fetchNextPage]);
 
   const renderGameCards = () => {
     if (isLoading) {
-      return Array.from({ length: 15 }).map((_, index) => <GameCardSkeleton key={index} />);
+      return Array.from({ length: 12 }).map((_, index) => <GameCardSkeleton key={index} />);
     }
 
-    return data?.results.map(game => <GameCard key={game.id} game={game} />);
+    return data?.pages.flatMap(
+      page => page.results.map(game => <GameCard key={game.id} game={game} />)
+    );
   }
 
   return (
@@ -25,6 +50,9 @@ function GameGrid({ gameQuery }: Props) {
       <SimpleGrid templateColumns='repeat(auto-fill, minmax(300px, 1fr))' spacing={3}>
         {renderGameCards()}
       </SimpleGrid>
+      <HStack justifyContent='center' py={10}>
+        <Spinner ref={loadMoreRef} size='lg' />
+      </HStack>
     </Box>
   );
 }

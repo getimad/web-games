eaf2fb9e3798b48c72ca4a2d58375e3aeb4f0998
Imad Ez-zahi <107067409+getimad@users.noreply.github.com>
2024-01-16 12:03:41 +0100

    Add ScrollToTopButton component to App.tsx

--- web-games.client/src/App.tsx
+++ web-games.client/src/App.tsx
@@ -6,6 +6,7 @@ import GenreList from './components/GenreList';
 import GameBar from './components/GameBar';
 import GameQuery from './interfaces/GameQuery';
 import GameHeading from './components/GameHeading';
+import ScrollToTopButton from './components/ScrollToTopButton';
 
 const NUMBER_OF_GAMES_PER_PAGE = 12;
 
@@ -13,31 +14,34 @@ function App() {
   const [gameQuery, setGameQuery] = useState<GameQuery>({} as GameQuery);
 
   return (
-    <Grid
-      templateAreas={{
-        base: `'nav' 'main'`,
-        lg: `'nav nav' 'aside main'`
-      }}
-      gridTemplateColumns={{
-        base: '1fr',
-        lg: '250px 1fr'
-      }}
-      gap={5}
-    >
-      <GridItem area='nav' position='fixed' w='100%' zIndex='200'>
-        <NavBar />
-      </GridItem>
-      <Show above='lg'>
-        <GridItem area='aside' margin={[null, null, null, '50px 0 0 20px']}>
-          <GenreList selectedGenre={gameQuery.genre} onSelectGenre={genre => setGameQuery({ ...gameQuery, genre })} />
+    <>
+      <Grid
+        templateAreas={{
+          base: `'nav' 'main'`,
+          lg: `'nav nav' 'aside main'`
+        }}
+        gridTemplateColumns={{
+          base: '1fr',
+          lg: '250px 1fr'
+        }}
+        gap={5}
+      >
+        <GridItem area='nav' position='fixed' w='100%' zIndex='200'>
+          <NavBar />
         </GridItem>
-      </Show>
-      <GridItem area='main' margin={['50px 20px 0 20px', null, null, '50px 20px 0 0']}>
-        <GameHeading gameQuery={gameQuery} />
-        <GameBar selectedPlatform={gameQuery.platform} onSelectPlatform={platform => setGameQuery({ ...gameQuery, platform })} selectedOrder={gameQuery.order} onSelectOrder={order => setGameQuery({ ...gameQuery, order })} />
-        <GameGrid gameQuery={gameQuery} pageSize={NUMBER_OF_GAMES_PER_PAGE} />
-      </GridItem>
-    </Grid>
+        <Show above='lg'>
+          <GridItem area='aside' margin={[null, null, null, '50px 0 0 20px']}>
+            <GenreList selectedGenre={gameQuery.genre} onSelectGenre={genre => setGameQuery({ ...gameQuery, genre })} />
+          </GridItem>
+        </Show>
+        <GridItem area='main' margin={['50px 20px 0 20px', null, null, '50px 20px 0 0']}>
+          <GameHeading gameQuery={gameQuery} />
+          <GameBar selectedPlatform={gameQuery.platform} onSelectPlatform={platform => setGameQuery({ ...gameQuery, platform })} selectedOrder={gameQuery.order} onSelectOrder={order => setGameQuery({ ...gameQuery, order })} />
+          <GameGrid gameQuery={gameQuery} pageSize={NUMBER_OF_GAMES_PER_PAGE} />
+        </GridItem>
+      </Grid>
+      <ScrollToTopButton />
+    </>
   );
 }
 

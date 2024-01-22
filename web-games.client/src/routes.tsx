229abe8fa2806c522d129aa7f06b084f37cbc09c
Imad Ez-zahi <107067409+getimad@users.noreply.github.com>
2024-01-22 11:41:33 +0100

    Add ErrorPage page to routes.tsx

--- web-games.client/src/routes.tsx
+++ web-games.client/src/routes.tsx
@@ -2,11 +2,13 @@ import { createBrowserRouter } from "react-router-dom";
 import LayoutPage from "./pages/LayoutPage";
 import HomePage from "./pages/HomePage";
 import GameDetailPage from "./pages/GameDetailPage";
+import ErrorPage from "./pages/ErrorPage";
 
 const MainRoutes = createBrowserRouter([
   {
     path: '/',
     element: <LayoutPage />,
+    errorElement: <ErrorPage />,
     children: [
       { index: true, element: <HomePage /> },
       { path: 'games/:id', element: <GameDetailPage /> }

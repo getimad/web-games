20c511a3ffd6aa5bc6493a340debec9becbe1d18
Imad Ez-zahi <107067409+getimad@users.noreply.github.com>
2024-01-22 05:30:43 +0100

    Add RouterProvider to main.tsx

--- web-games.client/src/main.tsx
+++ web-games.client/src/main.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { RouterProvider } from 'react-router-dom';
 import { ChakraProvider, ColorModeScript } from '@chakra-ui/react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import App from './App.tsx';
+import MainRoutes from './routes.tsx';
 import theme from './theme.ts';
 
 const query = new QueryClient();
@@ -12,7 +13,7 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
     <ChakraProvider theme={theme}>
       <ColorModeScript initialColorMode={theme.config.initialColorMode} />
       <QueryClientProvider client={query}>
-        <App />
+        <RouterProvider router={MainRoutes} />
       </QueryClientProvider>
     </ChakraProvider>
   </React.StrictMode>

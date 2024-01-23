29d01a7f375d80601d164dfb171b5f40835553bd
Imad Ez-zahi <107067409+getimad@users.noreply.github.com>
2024-01-23 20:17:14 +0100

    Add description, genres, release date, and website to Game interface

--- web-games.client/src/interfaces/Game.ts
+++ web-games.client/src/interfaces/Game.ts
@@ -1,12 +1,17 @@
+import Genre from './Genre';
 import Platform from './Platform';
 
 interface Game {
   id: number;
   slug: string;
   name: string;
+  description_raw: string;
   background_image: string;
+  genres: Genre[];
   parent_platforms: { platform: Platform }[];
   metacritic: number;
+  released: string;
+  website: string;
 }
 
 export default Game;

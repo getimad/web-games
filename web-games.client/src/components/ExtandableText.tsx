26ab17571b16f817ec0cdab71b35836599738a48
Imad Ez-zahi <107067409+getimad@users.noreply.github.com>
2024-01-24 18:18:56 +0100

    Add title prop to ExtandableText component

--- web-games.client/src/components/ExtandableText.tsx
+++ web-games.client/src/components/ExtandableText.tsx
@@ -1,19 +1,21 @@
-import { Box, Button, Text } from '@chakra-ui/react';
 import { useState } from 'react';
+import { Box, Button, Heading, Text } from '@chakra-ui/react';
 
 const limit = 350;  // LENGTH OF THE TEXT
 
 interface Props {
+  title?: string;
   children: string;
 }
 
-function ExtandableText({ children }: Props) {
+function ExtandableText({ title, children }: Props) {
   const [isExtandable, setIsExtandable] = useState(false);
 
   const content = isExtandable ? children : (children.substring(0, limit) + '...');
 
   return (
     <Box>
+      <Heading as='h6' size='md' marginBottom='1rem'>{title}</Heading>
       <Text>
         {content}
       </Text>

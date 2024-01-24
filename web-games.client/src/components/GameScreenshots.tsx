a579fc602e309a7a336d5e798ba8a80fed38ec82
Imad Ez-zahi <107067409+getimad@users.noreply.github.com>
2024-01-24 13:17:44 +0100

    style: Add scrollable list and styling to GameScreenshots component

--- web-games.client/src/components/GameScreenshots.tsx
+++ web-games.client/src/components/GameScreenshots.tsx
@@ -1,5 +1,5 @@
 import useScreeshots from "../hooks/useScreenshots";
-import { Image } from "@chakra-ui/react";
+import { Box, Image, List, Text } from "@chakra-ui/react";
 import getCroppedImageUrl from "../services/get-cropped-image-url";
 
 interface Props {
@@ -9,9 +9,26 @@ interface Props {
 function GameScreeshots({ gameId }: Props) {
   const { data, error, isLoading } = useScreeshots(gameId);
   return (
-    <>
-      {data?.results.map(i => <Image key={i.id} src={getCroppedImageUrl(i.image)} />)}
-    </>
+    <Box position='fixed'>
+      <Box position='absolute' w='100%' h='1rem' zIndex='1' backdropFilter='saturate(180%) blur(5px)' bottom='-1px' />
+      <List
+        overflowY='scroll'
+        h='calc(102vh)'
+        css={{
+          '&::-webkit-scrollbar': {
+            display: 'none',
+          }
+        }}
+        marginRight='3rem'
+        marginTop='-5rem'
+        paddingTop='6rem'
+        paddingBottom='1rem'
+      >
+        {data?.results.map(i =>
+          <Image key={i.id} src={getCroppedImageUrl(i.image)} borderRadius='1rem' marginBottom='1rem' />
+        )}
+      </List>
+    </Box>
   );
 }
 

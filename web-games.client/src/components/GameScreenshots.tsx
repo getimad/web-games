d63c238d65711806b09eeb90848c8c7174cc8d3a
Imad Ez-zahi <107067409+getimad@users.noreply.github.com>
2024-01-24 14:05:38 +0100

    style: Refactor GameScreenshots component
    Make the screenshots not draggable and refactor code.

--- web-games.client/src/components/GameScreenshots.tsx
+++ web-games.client/src/components/GameScreenshots.tsx
@@ -1,16 +1,24 @@
-import useScreeshots from "../hooks/useScreenshots";
-import { Box, Image, List, Text } from "@chakra-ui/react";
-import getCroppedImageUrl from "../services/get-cropped-image-url";
+import useScreeshots from '../hooks/useScreenshots';
+import { Box, Image, List } from '@chakra-ui/react';
+import getCroppedImageUrl from '../services/get-cropped-image-url';
 
 interface Props {
   gameId: number;
 }
 
 function GameScreeshots({ gameId }: Props) {
-  const { data, error, isLoading } = useScreeshots(gameId);
+  const { data } = useScreeshots(gameId);
+
   return (
     <Box position='fixed'>
-      <Box position='absolute' w='100%' h='1rem' zIndex='1' backdropFilter='saturate(180%) blur(5px)' bottom='-1px' />
+      <Box
+        position='absolute'
+        w='100%'
+        h='1rem'
+        zIndex='1'
+        backdropFilter='saturate(180%) blur(5px)'
+        bottom='-1px'
+      />
       <List
         overflowY='scroll'
         h='calc(102vh)'
@@ -25,7 +33,13 @@ function GameScreeshots({ gameId }: Props) {
         paddingBottom='1rem'
       >
         {data?.results.map(i =>
-          <Image key={i.id} src={getCroppedImageUrl(i.image)} borderRadius='1rem' marginBottom='1rem' />
+          <Image
+            key={i.id}
+            src={getCroppedImageUrl(i.image)}
+            draggable='false'
+            borderRadius='1rem'
+            marginBottom='1rem'
+          />
         )}
       </List>
     </Box>

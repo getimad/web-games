3d41f3857bbd50e6e96242e6de56eae96c852af5
Imad Ez-zahi <107067409+getimad@users.noreply.github.com>
2024-01-24 18:27:47 +0100

    style: Remove glass bar from GenreList.tsx

--- web-games.client/src/components/GenreList.tsx
+++ web-games.client/src/components/GenreList.tsx
@@ -23,8 +23,7 @@ function GenreList() {
       <Box backgroundColor='Gray.50' w='100%' h='60px' zIndex='300'>
         <Text fontSize='3xl' fontWeight={700}>Genres</Text>
       </Box>
-      <Box position='absolute' w='100%' h='1rem' zIndex='300' backdropFilter='saturate(180%) blur(5px)' bottom='0' />
-      <List overflowY='scroll' h='calc(100vh - 8rem)' paddingBottom='1rem' css={{
+      <List overflowY='scroll' h='calc(100vh - 8rem)' css={{
         '&::-webkit-scrollbar': {
           display: 'none',
         }

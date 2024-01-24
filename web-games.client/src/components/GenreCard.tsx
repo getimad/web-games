b55576f8c93fa060b836c4a246b6086ed1833177
Imad Ez-zahi <107067409+getimad@users.noreply.github.com>
2024-01-24 01:30:46 +0100

    Add navigation functionality and marginB to GenreCard
    This will return to the home page when click on genre card.

--- web-games.client/src/components/GenreCard.tsx
+++ web-games.client/src/components/GenreCard.tsx
@@ -2,6 +2,7 @@ import { HStack, ListItem, Image, Text, Button } from '@chakra-ui/react';
 import Genre from '../interfaces/Genre';
 import getCroppedImageUrl from '../services/get-cropped-image-url';
 import useGameQueryStore from '../store';
+import { useNavigate } from 'react-router-dom';
 
 interface Props {
   genre: Genre;
@@ -9,15 +10,17 @@ interface Props {
 
 function GenreCard({ genre }: Props) {
   const { gameQuery, setGenreId } = useGameQueryStore(s => ({ gameQuery: s.gameQuery, setGenreId: s.setGenreId }));
+  const navigate = useNavigate();
 
   const onGenreClick = () => {
     window.scrollTo({ top: 0, behavior: 'smooth' });
     setGenreId(genre.id);
+    navigate('/');
   };
 
   return (
     <ListItem>
-      <Button variant={gameQuery.genreId === genre.id ? 'solid' : 'ghost'} w='100%' justifyContent='left' paddingY={7} onClick={onGenreClick}>
+      <Button variant={gameQuery.genreId === genre.id ? 'solid' : 'ghost'} w='100%' justifyContent='left' paddingY={7} marginBottom='0.2rem' onClick={onGenreClick}>
         <HStack>
           <Image src={getCroppedImageUrl(genre.image_background)} boxSize={10} borderRadius={7} objectFit='cover' />
           <Text fontSize='sm' fontWeight='700'>{genre.name}</Text>

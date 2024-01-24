c3b37f5927aa3d195133781d050a32d84408c1b7
Imad Ez-zahi <107067409+getimad@users.noreply.github.com>
2024-01-24 01:02:54 +0100

    Add link to logo in NavBar component

--- web-games.client/src/components/NavBar.tsx
+++ web-games.client/src/components/NavBar.tsx
@@ -1,4 +1,5 @@
 import { Box, Flex, Image, Spacer, useColorMode } from '@chakra-ui/react';
+import { Link } from 'react-router-dom';
 import whiteLogo from '../assets/logo-white.svg';
 import blackLogo from '../assets/logo-black.svg';
 import ToggleThemeSwitch from './ToggleThemeSwitch';
@@ -9,7 +10,9 @@ function NavBar() {
   return (
     <Box backgroundColor={colorMode === 'dark' ? 'oGray.900' : 'oGray.50'} backdropFilter='saturate(180%) blur(10px)' position='fixed' w='100%' zIndex='200'>
       <Flex marginX='3rem' h={16} alignItems='center' justifyContent='space-between'>
-        <Image src={colorMode === 'dark' ? whiteLogo : blackLogo} boxSize={9} />
+        <Link to='/'>
+          <Image src={colorMode === 'dark' ? whiteLogo : blackLogo} boxSize={9} />
+        </Link>
         <Spacer />
         <ToggleThemeSwitch />
       </Flex>

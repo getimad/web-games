8fb7fce68f643b80c10b888136660a8d5998d59b
Imad Ez-zahi <107067409+getimad@users.noreply.github.com>
2024-01-24 22:28:20 +0100

    Add limit to search query for page_size param

--- web-games.client/src/hooks/useSearch.ts
+++ web-games.client/src/hooks/useSearch.ts
@@ -3,13 +3,15 @@ import Game from '../interfaces/Game';
 import FetchResponse from '../interfaces/FetchResponse';
 import APIClient from '../services/api-client';
 
+const limit = 12;
 const apiClient = new APIClient<Game>('/games');
 
 const useSearch = (searchQuery: string) => useQuery<FetchResponse<Game>>({
   queryKey: ['searchGames', searchQuery],
   queryFn: () => apiClient.getAll({
     params: {
-      search: searchQuery
+      search: searchQuery,
+      page_size: limit
     }
   }),
   staleTime: 0,

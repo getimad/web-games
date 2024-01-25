453602b58a61c0d028443415ca13e0a3fe3e88b5
Imad Ez-zahi <107067409+getimad@users.noreply.github.com>
2024-01-25 21:36:45 +0100

    Add goToHomePage functionality in NavBar component

--- web-games.client/src/components/NavBar.tsx
+++ web-games.client/src/components/NavBar.tsx
@@ -7,10 +7,17 @@ import SearchContainer from './SearchContainer';
 import useSearchStore from '../useSearchStore';
 import whiteLogo from '../assets/logo-white.svg';
 import blackLogo from '../assets/logo-black.svg';
+import useGameQueryStore from '../store';
 
 function NavBar() {
   const { colorMode } = useColorMode();
   const showSearchBox = useSearchStore(s => s.showSearchBox);
+  const setInitgameQuery = useGameQueryStore(s => s.setInitgameQuery);
+
+  const goToHomePage = () => {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+    setInitgameQuery();
+  }
 
   useEffect(() => {
     document.body.style.overflow = showSearchBox ? 'hidden' : 'visible';
@@ -21,13 +28,14 @@ function NavBar() {
       <Box backgroundColor={colorMode === 'dark' ? 'oGray.900' : 'oGray.50'} backdropFilter='saturate(180%) blur(10px)' position='fixed' w='100%' zIndex='200'>
         <Flex marginX='3rem' h='4rem' alignItems='center' justifyContent='space-between'>
           <Link to='/'>
-            <Image src={colorMode === 'dark' ? whiteLogo : blackLogo} boxSize={9} />
+            <Image src={colorMode === 'dark' ? whiteLogo : blackLogo} boxSize={9} onClick={goToHomePage} />
           </Link>
           <SearchButton />
           <ToggleThemeSwitch />
         </Flex>
-      </Box>
-      {showSearchBox && <SearchContainer />}
+      </Box >
+      {showSearchBox && <SearchContainer />
+      }
     </>
   );
 }

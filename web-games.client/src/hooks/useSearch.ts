84b3dd4419e19140df5acc68f8226ec40537a352
Imad Ez-zahi <107067409+getimad@users.noreply.github.com>
2024-01-25 17:28:20 +0100

    refactore: Add delay to useSearch hook for sending new request
    - Update gcTime property to 0 in useSearch hook.

--- web-games.client/src/hooks/useSearch.ts
+++ web-games.client/src/hooks/useSearch.ts
@@ -3,23 +3,34 @@ import Game from '../interfaces/Game';
 import FetchResponse from '../interfaces/FetchResponse';
 import APIClient from '../services/api-client';
 import useSearchStore from '../useSearchStore';
+import { useDebouncedCallback } from 'use-debounce';
+import { useEffect, useState } from 'react';
 
 const limit = 9;
 const apiClient = new APIClient<Game>('/games');
 
 const useSearch = () => {
   const searchQuery = useSearchStore(s => s.searchQuery);
+  const [debouncedSearchQuery, setDebouncedSearchQuery] = useState(searchQuery);
+
+  const debounceCallback = useDebouncedCallback((query: string) => {
+    setDebouncedSearchQuery(query);
+  }, 500);
+
+  useEffect(() => {
+    debounceCallback(searchQuery);
+  }, [searchQuery, debounceCallback]);
 
   return useQuery<FetchResponse<Game>>({
-    queryKey: ['searchGames', searchQuery],
+    queryKey: ['searchGames', debouncedSearchQuery],
     queryFn: () => apiClient.getAll({
       params: {
-        search: searchQuery,
+        search: debouncedSearchQuery,
         page_size: limit
       }
     }),
     staleTime: 0,
-    gcTime: 60 * 1000  // 1 minute
+    gcTime: 0
   })
 };
 

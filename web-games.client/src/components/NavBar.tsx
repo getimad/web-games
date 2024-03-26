5fd62eab742748b05da7749fdf9a70edf07468ca
Imad Ez-zahi <107067409+getimad@users.noreply.github.com>
2024-03-26 17:08:11 +0000

    refactor: Change logo of the web app

--- web-games.client/src/components/NavBar.tsx
+++ web-games.client/src/components/NavBar.tsx
@@ -1,41 +1,57 @@
-import { useEffect } from 'react';
-import { Link } from 'react-router-dom';
-import { Box, Flex, Image, useColorMode } from '@chakra-ui/react';
-import ToggleThemeSwitch from './ToggleThemeSwitch';
-import SearchButton from './SearchButton';
-import SearchContainer from './SearchContainer';
-import useSearchStore from '../useSearchStore';
-import whiteLogo from '../assets/logo-white.svg';
-import blackLogo from '../assets/logo-black.svg';
-import useGameQueryStore from '../store';
+import { useEffect } from "react";
+import { Link } from "react-router-dom";
+import { Box, Flex, Image, useColorMode } from "@chakra-ui/react";
+import ToggleThemeSwitch from "./ToggleThemeSwitch";
+import SearchButton from "./SearchButton";
+import SearchContainer from "./SearchContainer";
+import useSearchStore from "../useSearchStore";
+import whiteLogo from "../assets/white-logo.svg";
+import blackLogo from "../assets/black-logo.svg";
+import useGameQueryStore from "../store";
 
 function NavBar() {
   const { colorMode } = useColorMode();
-  const showSearchBox = useSearchStore(s => s.showSearchBox);
-  const setInitgameQuery = useGameQueryStore(s => s.setInitgameQuery);
+  const showSearchBox = useSearchStore((s) => s.showSearchBox);
+  const setInitgameQuery = useGameQueryStore((s) => s.setInitgameQuery);
 
   const goToHomePage = () => {
-    window.scrollTo({ top: 0, behavior: 'smooth' });
+    window.scrollTo({ top: 0, behavior: "smooth" });
     setInitgameQuery();
-  }
+  };
 
   useEffect(() => {
-    document.body.style.overflow = showSearchBox ? 'hidden' : 'visible';
+    document.body.style.overflow = showSearchBox ? "hidden" : "visible";
   }, [showSearchBox]);
 
   return (
     <>
-      <Box backgroundColor={colorMode === 'dark' ? 'oGray.900' : 'oGray.50'} backdropFilter='saturate(180%) blur(10px)' position='fixed' w='100%' zIndex='200'>
-        <Flex marginX='3rem' h='4rem' alignItems='center' justifyContent='space-between'>
-          <Link to='/'>
-            <Image src={colorMode === 'dark' ? whiteLogo : blackLogo} boxSize={9} onClick={goToHomePage} />
+      <Box
+        backgroundColor={colorMode === "dark" ? "oGray.900" : "oGray.50"}
+        backdropFilter="saturate(180%) blur(10px)"
+        position="fixed"
+        w="100%"
+        zIndex="200"
+      >
+        <Flex
+          marginX="3rem"
+          h="4rem"
+          alignItems="center"
+          justifyContent="space-between"
+        >
+          <Link to="/">
+            <Flex alignItems="center" gap="2">
+              <Image
+                src={colorMode === "dark" ? whiteLogo : blackLogo}
+                onClick={goToHomePage}
+                height={12}
+              />
+            </Flex>
           </Link>
           <SearchButton />
           <ToggleThemeSwitch />
         </Flex>
-      </Box >
-      {showSearchBox && <SearchContainer />
-      }
+      </Box>
+      {showSearchBox && <SearchContainer />}
     </>
   );
 }
